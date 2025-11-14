c42924dd68fb3f4a2c988c461c2a19cfd9b8e262
Abraham Bankole <abraham.o.bankole@gmail.com>
2025-11-14 19:42:15 +0100

    project scaffold

---
+++ src/main/java/com/abraham_bankole/runestone_bank/RuneStoneBankApplication.java
@@ -0,0 +1,13 @@
+package com.abraham_bankole.runestone_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuneStoneBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuneStoneBankApplication.class, args);
+    }
+
+}
