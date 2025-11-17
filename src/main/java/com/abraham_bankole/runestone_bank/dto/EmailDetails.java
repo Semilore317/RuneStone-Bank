8203314a512e03f59017aca4eea0661f2b207f84
Abraham Bankole <semi.banks.317@gmail.com>
2025-11-17 14:23:05 +0100

    implemented an email alert system

---
+++ src/main/java/com/abraham_bankole/runestone_bank/dto/EmailDetails.java
@@ -0,0 +1,19 @@
+package com.abraham_bankole.runestone_bank.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailDetails {
+    private String recipientEmail;
+    private String recipientName;
+
+    private String messageBody;
+    private String subject;
+    private String attachment;
+}
