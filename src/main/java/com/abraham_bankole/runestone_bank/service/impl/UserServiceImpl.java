73b59512d4a74bb0c33dc37cc667f2bd6f24a9a6
Abraham Bankole <semi.banks.317@gmail.com>
2025-11-17 12:31:24 +0100

    made the user creation api

--- src/main/java/com/abraham_bankole/runestone_bank/service/impl/UserServiceImpl.java
+++ src/main/java/com/abraham_bankole/runestone_bank/service/impl/UserServiceImpl.java
@@ -1,14 +1,34 @@
 package com.abraham_bankole.runestone_bank.service.impl;
 
+import com.abraham_bankole.runestone_bank.dto.AccountInfo;
 import com.abraham_bankole.runestone_bank.dto.BankResponse;
 import com.abraham_bankole.runestone_bank.dto.UserRequest;
 import com.abraham_bankole.runestone_bank.entity.User;
+import com.abraham_bankole.runestone_bank.repository.UserRepository;
+import com.abraham_bankole.runestone_bank.utils.AccountUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public BankResponse createAccount(UserRequest userRequest) {
         // create an account -> saving a new user into the db
+        // validate - check if user already exists
+        if(userRepository.existsByEmail(userRequest.getEmail())) {
+            return BankResponse.builder()
+                    .responseCode(AccountUtils.ACCOUNT_EXISTS_CODE)
+                    .responseMessage(AccountUtils.ACCOUNT_EXISTS_MESSAGE)
+                    .accountInfo(null)
+                    .build();
+        }
+
         User newUser = User.builder()
                 .firstName(userRequest.getFirstName())
                 .lastName(userRequest.getLastName())
@@ -16,7 +36,23 @@ public class UserServiceImpl implements UserService {
                 .gender(userRequest.getGender())
                 .address(userRequest.getAddress())
                 .stateOfOrigin(userRequest.getStateOfOrigin())
-                .accountNumber()
+                .accountNumber(AccountUtils.generateAccountNumber())
+                .accountBalance(BigDecimal.ZERO)
+                .email(userRequest.getEmail())
+                .phoneNumber(userRequest.getPhoneNumber())
+                .alternativePhoneNumber(userRequest.getAlternativePhoneNumber())
+                .status("ACTIVE")
+                .build();
+
+        User savedUser = userRepository.save(newUser);
+        return BankResponse.builder()
+                .responseCode(AccountUtils.ACCOUNT_CREATION_SUCCESS)
+                .responseMessage(AccountUtils.ACCOUNT_CREATION_MESSAGE)
+                .accountInfo(AccountInfo.builder()
+                        .accountNumber(savedUser.getAccountNumber())
+                        .accountBalance(savedUser.getAccountBalance())
+                        .accountName(savedUser.getFirstName() +  " " + savedUser.getLastName())
+                        .build())
                 .build();
     }
 }
--- src/main/java/com/abraham_bankole/runestone_bank/utils/AccountUtils.java
+++ src/main/java/com/abraham_bankole/runestone_bank/utils/AccountUtils.java
@@ -1,6 +1,26 @@
 package com.abraham_bankole.runestone_bank.utils;
 
-// method to generate an account number
-public class AccountUtils {
+import java.time.Year;
 
+// method to generate an account number
+// quite primitive at the moment
+// - doesnt take "buckets" into account, just creates a random 10 digit number
+public class AccountUtils {
+    public static final String ACCOUNT_EXISTS_CODE = "001";
+    public static final String ACCOUNT_EXISTS_MESSAGE = "This user email already exists";
+    public static final String ACCOUNT_CREATION_SUCCESS = "002";
+    public static final String ACCOUNT_CREATION_MESSAGE = "Account has been successfully created";
+
+    public static String generateAccountNumber() {
+        Year currentYear = Year.now();
+
+        int min = 100000;
+        int max = 999999;
+
+        int randomNumber = (int) Math.floor((Math.random() * (max - min + 1) + min));
+
+        String year = String.valueOf(currentYear);
+
+        return year + randomNumber;
+    }
 }
