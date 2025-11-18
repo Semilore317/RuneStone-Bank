403e78638c8b9328863dd5bab7fc6e5053e9675d
Abraham Bankole <abraham.o.bankole@gmail.com>
2025-11-18 12:26:11 +0100

    implemented name and balance enquiry

--- src/main/java/com/abraham_bankole/runestone_bank/repository/UserRepository.java
+++ src/main/java/com/abraham_bankole/runestone_bank/repository/UserRepository.java
@@ -4,5 +4,7 @@ import com.abraham_bankole.runestone_bank.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByAccountNumber(String accountNumber);
+    User findByAccountNumber(String accountNumber);
 }
--- src/main/java/com/abraham_bankole/runestone_bank/service/impl/UserService.java
+++ src/main/java/com/abraham_bankole/runestone_bank/service/impl/UserService.java
@@ -1,8 +1,11 @@
 package com.abraham_bankole.runestone_bank.service.impl;
 
 import com.abraham_bankole.runestone_bank.dto.BankResponse;
+import com.abraham_bankole.runestone_bank.dto.EnquiryRequest;
 import com.abraham_bankole.runestone_bank.dto.UserRequest;
 
 public interface UserService {
     BankResponse createAccount(UserRequest userRequest) ;
+    BankResponse balanceEnquiry(EnquiryRequest enquiryRequest) ;
+    BankResponse nameEnquiry(EnquiryRequest enquiryRequest) ;
 }
