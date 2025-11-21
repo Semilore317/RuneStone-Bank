4ee96feeb17cf4c4e7649ff845f6df738e175f79
Abraham Bankole <semi.banks.317@gmail.com>
2025-11-21 21:48:00 +0100

    customized swagger docs

--- src/main/java/com/abraham_bankole/runestone_bank/RuneStoneBankApplication.java
+++ src/main/java/com/abraham_bankole/runestone_bank/RuneStoneBankApplication.java
@@ -1,9 +1,35 @@
 package com.abraham_bankole.runestone_bank;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import jdk.jfr.Category;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "RuneStone Bank App",
+                description = "Backend REST APIs made with SpringBoot",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Abraham Bankole",
+                        email = "abraham.o.bankole@gmail.com",
+                        url = "https://github.com/Semilore317/RuneStone-Bank"
+                ),
+                license = @License(
+                        name = "RuneStone Bank",
+                        url = "https://github.com/Semilore317/RuneStone-Bank"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "RuneStone Bank",
+                url = "https://github.com/Semilore317/RuneStone-Bank"
+        )
+)
 public class RuneStoneBankApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/abraham_bankole/runestone_bank/controller/UserController.java
+++ src/main/java/com/abraham_bankole/runestone_bank/controller/UserController.java
@@ -2,15 +2,27 @@ package com.abraham_bankole.runestone_bank.controller;
 
 import com.abraham_bankole.runestone_bank.dto.*;
 import com.abraham_bankole.runestone_bank.service.impl.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(
+            summary = "Create New User Account",
+            description = "Create a user and assign a unique account number"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HttpS Status 201 CREATED"
+    )
     @PostMapping
     public BankResponse createAccount(@RequestBody UserRequest userRequest) {
         return userService.createAccount(userRequest);
