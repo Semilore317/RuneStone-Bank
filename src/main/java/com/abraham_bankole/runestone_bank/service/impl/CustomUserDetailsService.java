eb5a2bea8d3733d89c3b0a876dd2fdcace76c688
Abraham Bankole <semi.banks.317@gmail.com>
2025-11-30 21:15:17 +0100

    completed JwtAUthenticationFilter

---
+++ src/main/java/com/abraham_bankole/runestone_bank/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,13 @@
+package com.abraham_bankole.runestone_bank.service.impl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
