d62c6b48b683b30f9513f98e2da21202a1b22b7b
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-03-02 21:20:30 +0100

    added swagger annotations for endpoint details

--- src/main/java/com/abraham_bankole/runestone_bank/controller/TransactionController.java
+++ src/main/java/com/abraham_bankole/runestone_bank/controller/TransactionController.java
@@ -8,6 +8,9 @@ import jakarta.mail.MessagingException;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.io.FileNotFoundException;
 import java.time.LocalDate;
@@ -17,14 +20,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/bankstatement")
 @AllArgsConstructor
+@Tag(name = "Bank Statement APIs")
 public class TransactionController {
 
     private BankStatement bankStatement;
 
+    @Operation(summary = "Generate Bank Statement", description = "Generate and email bank statement for a given account and date range")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
     @GetMapping
     public List<Transaction> generateBankStatement(@RequestParam String accountNumber,
-                                                   @RequestParam String start,
-                                                   @RequestParam String end)
+            @RequestParam String start,
+            @RequestParam String end)
             throws DocumentException, FileNotFoundException, MessagingException {
         return bankStatement.generateStatement(accountNumber, start, end);
     }
