38a5d9e4da36853e2ee08381e84109b5b01d6e6a
Semilore317 <abraham.o.bankole@gmail.com>
2026-03-04 20:04:22 +0100

    refactor(api): decouple transaction and user domains
    - Extracted credit, debit, and transfer endpoints from UserController into a new TransactionController.
    - Updated API routing from `/api/user/*` to `/api/transactions/*` to align with RESTful resource conventions.
    - Created a new BankStatementController to handle bank statements instead of using the TransactionController

--- server/src/main/java/com/abraham_bankole/runestone_bank/controller/TransactionController.java
+++ server/src/main/java/com/abraham_bankole/runestone_bank/controller/TransactionController.java
@@ -1,33 +1,44 @@
 package com.abraham_bankole.runestone_bank.controller;
 
-import com.abraham_bankole.runestone_bank.entity.Transaction;
-import com.abraham_bankole.runestone_bank.service.impl.BankStatementService;
-import com.itextpdf.text.DocumentException;
-import jakarta.mail.MessagingException;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import com.abraham_bankole.runestone_bank.dto.BankResponse;
+import com.abraham_bankole.runestone_bank.dto.CreditDebitRequest;
+import com.abraham_bankole.runestone_bank.dto.TransferRequest;
+import com.abraham_bankole.runestone_bank.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
-import java.io.FileNotFoundException;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/bankstatement")
-@AllArgsConstructor
-@Tag(name = "Bank Statement APIs")
+@RequestMapping("/api/transactions")
+@Tag(name = "Transaction APIs")
 public class TransactionController {
 
-    private BankStatementService bankStatement;
+    @Autowired
+    private UserService userService;
 
-    @Operation(summary = "Generate Bank Statement", description = "Generate and email bank statement for a given account and date range")
+    @Operation(summary = "Credit Account", description = "Credit an account with a specific amount")
     @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
-    @GetMapping
-    public List<Transaction> generateBankStatement(@RequestParam String accountNumber,
-            @RequestParam String start,
-            @RequestParam String end)
-            throws DocumentException, FileNotFoundException, MessagingException {
-        return bankStatement.generateStatement(accountNumber, start, end);
+    @PostMapping("/credit")
+    public BankResponse credit(@RequestBody CreditDebitRequest creditRequest) {
+        return userService.creditAccount(creditRequest);
+    }
+
+    @Operation(summary = "Debit Account", description = "Debit an account with a specific amount")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
+    @PostMapping("/debit")
+    public BankResponse debit(@RequestBody CreditDebitRequest creditRequest) {
+        return userService.debitAccount(creditRequest);
+    }
+
+    @Operation(summary = "Transfer Funds", description = "Transfer funds from one account to another")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 SUCCESS")
+    @PostMapping("/transfer")
+    public BankResponse transfer(@RequestBody TransferRequest transferRequest) {
+        return userService.transfer(transferRequest);
     }
 }
