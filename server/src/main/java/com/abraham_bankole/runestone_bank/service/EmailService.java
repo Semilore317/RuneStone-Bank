e76550fa2465a7d44ab6f8d7056afd36fb38e5a9
Semilore317 <abraham.o.bankole@gmail.com>
2026-03-04 21:28:03 +0100

    chore: refactored all classes into their respective domains

--- server/src/main/java/com/abraham_bankole/runestone_bank/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package com.abraham_bankole.runestone_bank.service;
-
-import com.abraham_bankole.runestone_bank.dto.EmailDetails;
-
-public interface EmailService {
-  void sendEmailAlert(EmailDetails emailDetails);
-}
