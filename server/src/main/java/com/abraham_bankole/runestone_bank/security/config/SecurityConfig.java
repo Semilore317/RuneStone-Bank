14aa176b1187d485d9b34fdc6bda1d74e5a44f09
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-03-17 13:32:25 +0100

    chore: removed typo in SecurityConfig

--- server/src/main/java/com/abraham_bankole/runestone_bank/security/config/SecurityConfig.java
+++ server/src/main/java/com/abraham_bankole/runestone_bank/security/config/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);uu
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
