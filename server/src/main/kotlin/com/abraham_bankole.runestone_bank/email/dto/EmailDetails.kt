e34ec17548ec01d6483bb730dc2b752b11a9ea6b
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-04-03 15:45:29 +0100

    fix: rectified flat directory issue in email service

--- server/src/main/kotlin/com/abraham_bankole.runestone_bank/email/dto/EmailDetails.kt
+++
@@ -1,9 +0,0 @@
-package com.abraham_bankole.runestone_bank.email.dto
-
-data class EmailDetails (
-    var recipientEmail: String = "",
-    var recipientName: String = "",
-    var messageBody: String = "",
-    var subject: String = "",
-    var attachment: String? = null
-)
