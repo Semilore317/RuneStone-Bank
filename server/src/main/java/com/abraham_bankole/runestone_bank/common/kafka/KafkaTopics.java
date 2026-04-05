a36b393f8bf6a50e6e73d061ad23f16705c76e44
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-04-05 19:58:43 +0100

    feat: added KafkaTopics class for centralized topic names

---
+++ server/src/main/java/com/abraham_bankole/runestone_bank/common/kafka/KafkaTopics.java
@@ -0,0 +1,10 @@
+package com.abraham_bankole.runestone_bank.common.kafka;
+
+public class KafkaTopics {
+    private KafkaTopics() {};
+
+    public static final String USER_REGISTERED = "user.registered";
+    public static final String USER_LOGIN = "user.login";
+    public static final String TRANSACTION_COMPLETED = "transaction.completed";
+    public static final String STATEMENT_READY = "statement.ready";
+}
