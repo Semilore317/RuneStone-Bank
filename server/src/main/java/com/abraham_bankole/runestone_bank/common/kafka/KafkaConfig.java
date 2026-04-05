c8971c5890456fc4a46d0f9b5c56bdb0c12bd192
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-04-05 20:16:19 +0100

    feat: scaffolded KafkaConfig file

---
+++ server/src/main/java/com/abraham_bankole/runestone_bank/common/kafka/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.abraham_bankole.runestone_bank.common.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic userRegisteredTopic(){
+        return TopicBuilder
+                .name(KafkaTopics.USER_REGISTERED)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+}
