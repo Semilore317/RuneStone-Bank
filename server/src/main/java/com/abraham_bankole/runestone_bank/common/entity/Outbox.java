eda0494ca313dd0c9ac37a2bd5c8ef74a5ac3055
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-04-07 21:31:56 +0100

    feat: added Outbox entity in common domain

---
+++ server/src/main/java/com/abraham_bankole/runestone_bank/common/entity/Outbox.java
@@ -0,0 +1,36 @@
+package com.abraham_bankole.runestone_bank.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Outbox {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String aggregateId;
+
+    @Column(nullable = false)
+    private String aggregateType;
+
+    @Column(nullable = false)
+    private String eventType;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String payload;
+
+    @Builder.Default
+    private Instant createdAt = Instant.now();
+}
