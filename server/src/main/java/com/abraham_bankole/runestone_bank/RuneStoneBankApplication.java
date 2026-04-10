3788bf41eafd4c31ffa8051057381648814a5a78
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-04-10 19:32:43 +0100

    feat: registered UUID as a class requiring reflection to appease GraalVM

--- server/src/main/java/com/abraham_bankole/runestone_bank/RuneStoneBankApplication.java
+++ server/src/main/java/com/abraham_bankole/runestone_bank/RuneStoneBankApplication.java
@@ -1,5 +1,6 @@
 package com.abraham_bankole.runestone_bank;
 
+import com.abraham_bankole.runestone_bank.common.config.ReflectionHints;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -8,32 +9,34 @@ import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@ImportRuntimeHints(ReflectionHints.class)
 @OpenAPIDefinition(
-    info =
+        info =
         @Info(
-            title = "RuneStone Bank App",
-            description = "Backend REST APIs made with SpringBoot",
-            version = "v1.0",
-            contact =
+                title = "RuneStone Bank App",
+                description = "Backend REST APIs made with SpringBoot",
+                version = "v1.0",
+                contact =
                 @Contact(
-                    name = "Abraham Bankole",
-                    email = "abraham.o.bankole@gmail.com",
-                    url = "https://github.com/Semilore317/RuneStone-Bank"),
-            license =
+                        name = "Abraham Bankole",
+                        email = "abraham.o.bankole@gmail.com",
+                        url = "https://github.com/Semilore317/RuneStone-Bank"),
+                license =
                 @License(
-                    name = "RuneStone Bank",
-                    url = "https://github.com/Semilore317/RuneStone-Bank")),
-    externalDocs =
+                        name = "RuneStone Bank",
+                        url = "https://github.com/Semilore317/RuneStone-Bank")),
+        externalDocs =
         @ExternalDocumentation(
-            description = "RuneStone Bank",
-            url = "https://github.com/Semilore317/RuneStone-Bank"))
+                description = "RuneStone Bank",
+                url = "https://github.com/Semilore317/RuneStone-Bank"))
 public class RuneStoneBankApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(RuneStoneBankApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(RuneStoneBankApplication.class, args);
+    }
 }
--- server/src/main/java/com/abraham_bankole/runestone_bank/common/config/ReflectionHints.java
+++ server/src/main/java/com/abraham_bankole/runestone_bank/common/config/ReflectionHints.java
@@ -1,13 +1,17 @@
 package com.abraham_bankole.runestone_bank.common.config;
 
 import org.jetbrains.annotations.Nullable;
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+
+import java.util.UUID;
 
 // GraalVM vs Spring-Boot reflections workaround
 public class ReflectionHints implements RuntimeHintsRegistrar {
     @Override
     public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
-
+        hints.reflection().registerType(UUID.class, MemberCategory.values());
+        hints.reflection().registerType(UUID[].class, MemberCategory.values());
     }
 }
