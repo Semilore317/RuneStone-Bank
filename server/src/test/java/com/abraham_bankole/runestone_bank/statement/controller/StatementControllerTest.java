a5e9067219a5138ae98ccb67a2167ed701f58f2f
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-04-26 20:38:27 +0100

    fix: removed unused import for anyString

--- server/src/test/java/com/abraham_bankole/runestone_bank/statement/controller/StatementControllerTest.java
+++ server/src/test/java/com/abraham_bankole/runestone_bank/statement/controller/StatementControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
