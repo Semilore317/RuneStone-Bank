a6ed0cf5fba26dabb9fb52808f8fc2da23f1565f
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-04-26 20:39:42 +0100

    fix: removed unused import for assertTrue

--- server/src/test/java/com/abraham_bankole/runestone_bank/transaction/repository/TransactionRepositoryTest.java
+++ server/src/test/java/com/abraham_bankole/runestone_bank/transaction/repository/TransactionRepositoryTest.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 class TransactionRepositoryTest {
