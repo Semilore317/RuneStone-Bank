9561c356d6416e74a9721ac0cd943e7925bf4039
Abraham Bankole <abraham.o.bankole@gmail.com>
2026-04-26 20:25:22 +0100

    fix: removed wildcard mockito import and replaced it with explicit imports

--- server/src/test/kotlin/com/abraham_bankole/runestone_bank/email/listener/EmailEventListenerTest.kt
+++ server/src/test/kotlin/com/abraham_bankole/runestone_bank/email/listener/EmailEventListenerTest.kt
@@ -10,7 +10,11 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito.*
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.times
+import org.mockito.Mockito.any
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.mail.javamail.JavaMailSender
 
